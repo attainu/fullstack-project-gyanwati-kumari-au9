e2b528adf50e3fa289c8e06322cb435d34f81c28
Gyan <gyan@Abhisheks-MacBook-Air-63.local>
2021-03-14 10:40:45 +0530

    search api

--- AgriBazzarApi/api.js
+++ AgriBazzarApi/api.js
@@ -1,167 +1,220 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const mongo = require('mongodb');
-const MongoClient = mongo.MongoClient
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongourl = "mongodb+srv://admin:AdminUser123@cluster0.i5gbr.mongodb.net/AgriBazzar?retryWrites=true&w=majority";
-let dbobj =  require('./dbconnection');
+const mongodb = require('mongodb');
+const {MongoClient} = require('mongodb');
+const uri = "mongodb+srv://admin:AdminUser123@cluster0.i5gbr.mongodb.net/AgriBazzar?retryWrites=true&w=majority";
 
-let col_name="users";
-
-//middleware
-//cross origin resource sharing
+const port = process.env.PORT || 5000;
+// DB Connection logic import
+const { initialize } = require('./dbconnection');
+//make object of express so that we can use methods
+let app = express();
+let dbobj;
 app.use(cors())
-
-//parse data for post call
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 
-//load home page
+initialize('AgriBazzar', (db)=>{
+    dbobj = db;
+},(err)=>{
+    console.log("Error while connecting to db", err)
+})
+
+
+
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+};
+
+
 app.get('/',(req,res) => {
-    dbobj(function(err,db){
-        db.collection('users').
-        find({}, function(err, result) {
-         if (err) {
-            console.log(err);
-           return;
-         } 
-         else {
-            return result;
-         }
-    });
-})
+    let errmessage = req.query.errmessage?req.query.errmessage:'';
+    let successmessage = req.query.sucmessage?req.query.sucmessage:'';
+    res.render('pages/login',{errmessage:errmessage,successmessage:successmessage})
 })
 
-app.get('/new',(req,res) => {
-    res.render('admin')
+app.get('/register',(req,res) => {
+    let errmessage = req.query.errmessage?req.query.errmessage:'';
+    res.render('pages/register',{errmessage:errmessage})
 })
 
-//callback
-app.get('/getuser',(req,res) => {
-    dbobj.collection(col_name).find({}).toArray((err,result) => {
-        if(err) throw err;
-        res.send(result)
-    })
-})
 
-//async await
-app.get('/getUser1', async(req,res)=>{
-    let response = await dbobj.collection(col_name).find({}).toArray(err,result);
-    res.send(response)
-})
-
-//postUser
-app.post('/adduser',(req,res)=>{
-    const data = {
-        "name": req.body.name,
-        "city": req.body.city,
-        "phone": req.body.phone,
-        "isActive": req.body.isActive?req.body.isActive:true,
-        "role": req.body.role?req.body.role:'User',
-        "email": req.body.email
+//Register User
+app.post('/register',(req,res) => {
+    let user = {
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        role:req.body.role?req.body.role:'user',
+        isActive:true
     }
-    dbobj.collection(col_name).insert(data,(err,result) => {
-        if(err) throw err;
-        //res.status(200).send("Data Added")
-        res.redirect('/')
-    });
+    dbobj.collection('users').findOne({email:user.email},(err,data) => {
+        if(data){
+            return res.redirect("/register?errmessage=Email Already Taken Added")  
+        }else{
+            dbobj.collection('users').insert(user,(err,data)=>{
+                if(err) throw err;
+                //return res.send("Data Added")
+                res.redirect("/?sucmessage=Successfully Register")  
+            })
+        }
+    })
 });
 
-//getUser
-app.get('/users',(req,res) => {
-    var query = {}
-    if(req.query.city && req.query.role){
-        query={city:req.query.city,role:req.query.role,isActive:true}
-    }
-    else if(req.query.city){
-        query={city:req.query.city,isActive:true}
-    }else if(req.query.role){
-        query={role:req.query.role,isActive:true}
+//editUser
+app.put('/edituser',(req,res) => {
+    console.log(req.body)
+    let status;
+    if(req.body.isActive){
+        if(req.body.isActive=='true'){
+            status=true
+        }else{
+            status=false
+        }
     }else{
-        query={isActive:true}
+        status=false
     }
-    dbobj.collection(col_name).find(query).toArray((err,result) => {
-        if(err) throw err;
-        res.send(result)
-    })
-})
 
-//UserDetails
-app.get('/user/:id',(req,res) => {
-    var id = mongo.ObjectID(req.params.id)
-    var query = {}
-    query={_id:id}
-    dbobj.collection(col_name).findOne(query,(err,result) => {
-        if(err) throw err;
-        res.send(result)
-    })
-})
-
-//updateUser
-app.put('/editUser',(req,res) => {
-    let Id = req.body._id;
-    dbobj.collection(col_name).update(
-        {_id:mongo.ObjectID(Id)},
+    dbobj.collection('users').update(
+        {_id:mongodb.ObjectID(req.body._id)},
         {
             $set:{
                 name:req.body.name,
                 email:req.body.email,
-                city: req.body.city,
-                phone: req.body.phone,
-                isActive:req.body.isActive?true:false,
-                role: req.body.role,
+                role:req.body.role?req.body.role:'user',
+                isActive:status
             }
-        },(err,result) => {
+        },(err,result) =>{
             if(err) throw err;
-            res.send("Data Updated")
+            res.send('Data Updates')
         }
     )
+});
+
+//login
+app.post('/login',(req,res) => {
+    let user={
+        email:req.body.email,
+        password:req.body.password
+    }
+ 
+    dbobj.collection('users').findOne(user,(err,data) => {
+        if(err || !data){
+            return res.redirect('/?errmessage=Invalid Login! Please Try Again')
+        }else{
+            //save data in session
+            /*if(req.session.user.email = data.email){
+                
+            }*/
+            req.session.user = data;
+            console.log(req.session.user)
+            console.log("login>>>",req.session)
+            //return res.send(data)
+            res.redirect('/post')
+        }
+    })
 })
 
-//hardDeleteUser
-app.delete('/deleteUser',(req,res) => {
-    let Id = mongo.ObjectID(req.body._id);
-    dbobj.collection(col_name).remove({_id:Id},(err,result) => {
-        if(err) throw err;
-        res.send("Data Deleted")
+app.get('/chlogin', (req,res)=>{
+    let errmessage = req.query.errmessage?req.query.errmessage:'';
+    let successmessage = req.query.sucmessage?req.query.sucmessage:'';
+    res.render('pages/change-password',{errmessage:errmessage, successmessage:successmessage})
+})
+// change password
+app.post('/chlogin',(req,res) => {
+    let user={
+        cpassword:req.body.cpassword,
+        password:req.body.password,
+        cnfpassword: req.body.cnfpassword,
+    }
+    if (!req.session.user){
+        return res.redirect('/chlogin?errmessage=You must be logged in to changed password')
+    }
+
+    if (req.body.cnfpassword !== req.body.password){
+        return res.redirect('/chlogin?errmessage=New and Confirm pssword should be same')
+    }
+    dbobj.collection('users').findOne({email: req.session.user.email},(err,data) => {
+        if(err || !data){
+            return res.redirect('/chlogin?errmessage=Invalid Login! Please Try Again')
+        }
+        else{
+            //save data in session
+            if (data.password == req.body.cpassword){
+                dbobj.collection('users').updateOne({email:req.body.email},{$set: {password: req.body.newPassword}})
+                res.redirect('/chlogin?successmessage=successfully updated')
+            }else {
+                return res.redirect('/chlogin?errmessage=Current password does not match, please enter correct password')
+            }
+            
+        }
+    })
+})
+
+//all users
+app.get('/allUsers',(req,res) => {
+    console.log("session>>>",req.session)
+    if(!req.session.user){
+        return res.redirect('/?errmessage=No Session Found! Please Login Again')
+    }
+    if(req.session.user.role !=="Admin" && req.session.user){
+        return res.redirect('/post?errmessage=You are Not Admin')
+    }
+    dbobj.collection('users').find().toArray((err,data)=>{
+        //return res.send(data)
+        res.render('users',{data:data,userdata:req.session.user})
     })
 });
 
-//deactivate User(soft Delete)
-app.put('/deactivateUser',(req,res)=>{
-    let Id = mongo.ObjectID(req.body._id);
-    dbobj.collection(col_name).update(
-        {_id:Id},
-        {
-            $set:{
-                isActive:false
-            }
-        },(err,result)=> {
-            if(err) throw err;
-            res.send("User Deactivated")
-        }
-    )
+//usersbyid
+app.get('/userbyid',(req,res) => {
+    console.log("session>>>",req.session)
+    if(!req.session.user){
+        return res.redirect('/?errmessage=No Session Found! Please Login Again')
+    }
+    if(req.session.user.role !=="Admin" && req.session.user){
+        return res.redirect('/post?errmessage=You are Not Admin')
+    }
+    let query ={}
+    if(req.query.id){
+        query={_id:mongodb.ObjectID(req.query.id)}
+    }
+    dbobj.collection('users').findOne(query,(err,data)=>{
+        return res.send(data)
+    })
+});
+
+
+//logout
+app.get('/logout',(req,res) => {
+    req.session.user = null;
+   //return res.send('Logout Success')
+   res.redirect('/')
 })
 
-//Activate User(soft Delete)
-app.put('/activateUser',(req,res)=>{
-    let Id = mongo.ObjectID(req.body._id);
-    dbobj.collection(col_name).update(
-        {_id:Id},
-        {
-            $set:{
-                isActive:true
-            }
-        },(err,result)=> {
-            if(err) throw err;
-            res.send("User Activated")
-        }
-    )
-})
+// search 
+app.get('/search', asyncMiddleware(async(req,res)=>{
+    let response = await dbobj.collection('search').find({}).toArray();
+    console.log(response);
+    res.send(response)
+}))
+
+
+
+
+
 
 app.listen(port,(err) => {
-    console.log(`Server is running on port ${port}`)
+    if(err) throw err;
+    console.log(`App is running on port ${port}`)
 })
+
+
+
+
+
+
--- AgriBazzarApi/dbconnection.js
+++ AgriBazzarApi/dbconnection.js
@@ -1,8 +1,26 @@
-var mongodb  = require('mongodb')
-function connection(callback){
-    var MongoClient = mongodb.MongoClient;
-    var url = "mongodb+srv://admin:AdminUser123@cluster0.i5gbr.mongodb.net/AgriBazzar?retryWrites=true&w=majority";
-    MongoClient.connect(url, callback)
+const {MongoClient} = require('mongodb');
+
+const uri = "mongodb+srv://admin:AdminUser123@cluster0.i5gbr.mongodb.net/AgriBazzar?retryWrites=true&w=majority";
+
+function initialize(
+    dbName,
+    successCallback,
+    failureCallback
+) {
+    MongoClient.connect(uri, function(err, dbInstance) {
+        if (err) {
+            console.log(`[MongoDB connection] ERROR: ${err}`);
+            failureCallback(err); // this should be "caught" by the calling function
+        } else {
+            const dbObject = dbInstance.db(dbName);
+            // const dbCollection = dbObject.collection(dbCollectionName);
+            console.log("[MongoDB connection] SUCCESS");
+
+            successCallback(dbObject);
+        }
+    });
 }
 
-module.exports = connection
+module.exports = {
+    initialize
+};
